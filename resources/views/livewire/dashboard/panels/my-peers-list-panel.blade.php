d213b5452cd10b9ac9238c98998200af1c851d1e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-10-10 11:07:12 -0700

    wip

--- resources/views/livewire/dashboard/panels/my-peers-list-panel.blade.php
+++ resources/views/livewire/dashboard/panels/my-peers-list-panel.blade.php
@@ -1,12 +1,19 @@
 <x-panel title="My Peers" :padding="false" icon="users">
                     
     <ul class="divide-y">
-        @foreach ($peers as $peer)
+
+        @forelse ($peers as $peer)
+
             <li class="flex items-center px-2 py-1.5 space-x-2">
                 <img src="{{ $peer->avatar_url }}" alt="{{ $peer->name }}'s avatar" class="w-6 h-6 rounded-full">
                 <span class="truncate">{{ $peer->name }}</span>
             </li>
-        @endforeach
+            
+        @empty
+
+            <li class="py-2 text-sm text-center text-gray-500 select-none">No peers yet</li>
+        
+        @endforelse
     </ul>
 
 </x-panel>
